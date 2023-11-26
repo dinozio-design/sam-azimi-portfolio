eea6143e869927a00f85c561e32379f053c1f2fb
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-11-26 13:06:37 -0500

    editing app with initial sections and styles this is just the overall feel and sett up of the app all styles are under the same css file for now will be moved to individual component modules once the over all is finalized

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import logo from './logo.svg';
+import './App.css';
+
+// add styles for header, body, and charly
+
+function App() {
+  return (
+    <>
+      {/* add Header */}
+      {/* add body */}
+      {/* ass Feed Charly */}
+      <div className="App">
+        <header className="App-header">
+          <h1>SAM AZIMI</h1>
+          <p>JavaScrip Developer</p>
+        </header>
+        <main className="App-body">
+          <section>About</section>
+          <section>Projects</section>
+          <section>Footer</section>
+        </main>
+      </div>
+    </>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+  );
+}
+
+export default App;
