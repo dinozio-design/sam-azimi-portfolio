f15212484b2fba5e6fc819ca5fa24bceb5c65c7a
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-11-27 18:31:26 -0500

    building the nav, one brick at a time

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,18 @@
 import React, { useState } from "react";
-import "./Navbar.css";
 
 export default function Navbar() {
     const [isNavOpen, setNavOpen] = useState(false);
 
-    const toggleNav = () => {
+    const toggleNav = ()=>{
         setNavOpen(!isNavOpen);
     };
-    const closeNav = () => {
+    const closeNav = ()=>{
         setNavOpen(false);
     };
 
     return (
-        <div className={`navbar ${isNavOpen ? 'open' : ''}`}>
-            <div className="hamburger" onClick={toggleNav}>
-                {isNavOpen ? <span>&larr; Close</span> : <span>&#9776;</span>}
-            </div>
-            <div className="nav-links">
-                <a href="#">Home</a>
-                <a href="#">About</a>
-                <a href="#">Contact</a>
-                {/* Add more links as needed */}
-                <button className="close-button" onClick={closeNav}>
-                    &larr;
-                </button>
-            </div>
+        <div className={"navbar"}>
+            
         </div>
     );
 };
