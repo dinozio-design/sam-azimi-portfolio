bc3eed022e11ba3417b0a7e7efecd96788882fd1
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-12-02 14:46:56 -0500

    initiating footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../Navbar";
+
+export default function Footer() {
+    return (
+        <>
+            <footer className="App-footer">
+                <h4>This is the footer</h4>
+                <p>All rights reserved</p>
+            </footer>
+        </>
+    );
+};
