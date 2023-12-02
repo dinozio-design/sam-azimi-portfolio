28862b3dede0329806bf1e0ac53dac0b303825ef
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-12-02 14:59:52 -0500

    initiated the resume section

--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import About from "../About";
 import Footer from "../../components/Footer";
 import Projects from "../Projects";
+import Resume from "../Resume";
 
 
 export default function MainPage() {
     return (
         <>
             <About />
-            <Projects/>
+            <Projects />
+            <Resume />
             <Footer />
         </>
     );
---
+++ src/pages/Resume/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Resume() {
+    return (
+        <>
+            <div> This is the Resume section</div>
+        </>
+    );
+};
