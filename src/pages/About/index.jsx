dd582b6776f34d12554c24a2018890b18e1b93df
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-12-04 16:09:52 -0500

    Started the About me and writing it one paragraph at a time

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -3,7 +3,20 @@ import React from "react";
 export default function About() {
     return (
         <>
-            <div> This is the about section</div>
+            <div> 
+                <h2>
+                    About Me
+                </h2>
+                <h4>My Name is Sam! ... and I am a JavaScript developer. </h4>
+                <p>I used to work in the Solar industry and at the begining of 2023 I decided to change my career path and become a developer. So I signed up and completed <strong>an intense bootcamp program</strong> offered at the University of Toronto (UofT) [add link] by 2U [add link]. The program was very tough and the learning journey required a lot of time and patience. It was extremely rewarding and I completed it with great confidence and many new expertiese in my tool-belt.You can see the list of my programming languages, frameworks, devops, and tools under "My Resume" section [add link]. </p>
+                <h3>My Story</h3>
+                <p>I am a professional engineer with many years of experience in multidisciplinary industries. I have always been very curius about how everything works, and always took the more difficult path to challenge myself and to learn. I believe the best way of learning is by doing. That is the reason I took on the challenge of Bootcamp so that I can put myself through the tough rigure of learning by doing. And I have come a long way from the first days of writing only HTML code ;)</p>
+                <h4>My Educations</h4>
+                <p>When I was in highschool, I always knew that I want to study engineering, but I wasn't sure which descipline of engineering would be more suitable for me. Last year of highschool I took an elective Co-op course where they thought us the meaning of being in the workforce, the way to write resumes, cover letters, </p>
+                <h4>My Work Experiences</h4>
+                <h4>My Personal Challenges</h4>
+
+            </div>
         </>
     );
 };
