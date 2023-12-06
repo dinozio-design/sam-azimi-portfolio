44ea4c850303813d4dc0a2430590f12bad2d2c5a
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-12-05 19:27:22 -0500

    starting to use the Card componenet

--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import Card from "../../layout/Card";
 
 export default function Projects() {
     return (
         <>
-            <div> This is the Projects section</div>
+            <div>
+                <h2>
+                    Projects
+                </h2>
+                <p>Below you can find a select list of the projects that I have created along with some details about them. All of these projects are open-source and if you follow the GitHub tab, you can find the repository that holds the source code for them. Some of these were class assignments and group projects, and some were just for fun.</p>
+                <Card heading = "Project 1">
+                    
+                </Card>
+            </div>
         </>
     );
 };
