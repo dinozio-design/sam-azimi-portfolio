955b7bcf3564a2014e1ccfbf1ff69102c5851d52
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-12-05 20:14:32 -0500

    project 2: weather api overall

--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -12,14 +12,33 @@ export default function Projects() {
                 <Card heading="Password Generator">
                     <p> This app is a great example of an Object Oriented JavaScript. Instead of using RegEx, I hardcoded every scenario (future refactoring is on the todo list to simplify the code with RegEx. It is in the que!). I used the window.prompt method to open the dialogue box and collect the userResponse for variety of options to generate a strong password. Nowadays every website or app has it's own requirements for a strong password that varries from lenght to Upper/Lowercase and alphanumeric or special characters. This app allows the user to chose any of the necessary requirements for a strong password. After the password is generated, the user then is able to copy the string and paste it to their own password keeper.</p>
                     <p>Image of the app goes here</p>
-                    <p>GitHub repo link</p>
+                    <p>GitHub repo link: https://github.com/dinozio-design/03-Password-Generator-JavaScript</p>
                     <p>GitHub icon</p>
-                    <p>deployed link</p>
+                    <p>deployed link: https://dinozio-design.github.io/03-Password-Generator-JavaScript/</p>
                     <p>deploy icon</p>
                     <button>JavaScrip</button>
                     <button>Object Oriented Programming (OOP)</button>
                     <button>CSS</button>
                     <button>Media Query</button>
+                    <button>Mobile First</button>
+                    <button>Responsive Design</button>
+                    <button>HTML</button>
+                </Card>
+                <Card heading = "Weather Dashboard">
+                    <p>This app uses Server Side API ... write more description</p>
+                    <p>Image of the app goes here</p>
+                    <p>GitHub repo link: https://github.com/dinozio-design/06-Weather-Dashboard-Server-Side-APIs</p>
+                    <p>GitHub icon</p>
+                    <p>deployed link: https://dinozio-design.github.io/06-Weather-Dashboard-Server-Side-APIs/</p>
+                    <p>deploy icon</p>
+                    <button>JavaScrip</button>
+                    <button>jQuery</button>
+                    <button>Functional Programming</button>
+                    <button>Local Storage</button>
+                    <button>RESTFull</button>
+                    <button>Third Party API</button>
+                    <button>Server Side API</button>
+                    <button>Bulma CSS</button>
                     <button>Mobile First</button>
                     <button>Responsive Design</button>
                     <button>HTML</button>
