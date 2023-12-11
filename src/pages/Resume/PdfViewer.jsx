8034b0fcb0a2413bcf5259a67e2e5c641688b7d6
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-12-11 17:12:57 -0500

    added code for pdfViewer

--- src/pages/Resume/PdfViewer.jsx
+++ src/pages/Resume/PdfViewer.jsx
@@ -1,10 +1,23 @@
 import React from "react";
+import { useState } from 'react';
 import { Document, Page } from "react-pdf";
 
-export default function PdfViewer({pdf}){
-    return(
-        <Document file={pdf}>
-            <Page pageNumber={1}/>
-        </Document>
+export default function PdfViewer({ pdf }) {
+    const [numPages, setNumPages] = useState();
+    const [pageNumber, setPageNumber] = useState(1);
+
+    function onDocumentLoadSuccess({ numPages }) {
+        setNumPages(numPages);
+    };
+
+    return (
+        <div>
+      <Document file="somefile.pdf" onLoadSuccess={onDocumentLoadSuccess}>
+        <Page pageNumber={pageNumber} />
+      </Document>
+      <p>
+        Page {pageNumber} of {numPages}
+      </p>
+    </div>
     );
 }
