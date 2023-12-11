056a88cc2ad5bbe1f34c0ac57e40b36e88309333
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-12-11 15:14:55 -0500

    summarized the aboutMeData

--- src/pages/About/MoreAboutMeCard.jsx
+++ src/pages/About/MoreAboutMeCard.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-export default function MoreAboutMeCard({ title, description }) {
+export default function MoreAboutMeCard({ title, summary }) {
     return (
         <div className="card">
             <div className="card-header text-center">
                 <h4>{title}</h4>
             </div>
             <div className="card-body">
-                <p>{description}</p>
+                <p>{summary}</p>
             </div>
         </div>
     );
