162075d92155d34b584c0c7553492820a44e6532
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-12-11 12:58:40 -0500

    password generator added

--- src/data/ProjectData.jsx
+++ src/data/ProjectData.jsx
@@ -1,11 +1,11 @@
 const ProjectData = [
     {
-        titel: "",
-        description: "",
+        titel: "Password Generator",
+        description: "This app is a great example of an Object Oriented JavaScript. Instead of using RegEx, I hardcoded every scenario (future refactoring is on the todo list to simplify the code with RegEx. It is in the que!). I used the window.prompt method to open the dialogue box and collect the userResponse for variety of options to generate a strong password. Nowadays every website or app has it's own requirements for a strong password that varries from lenght to Upper/Lowercase and alphanumeric or special characters. This app allows the user to chose any of the necessary requirements for a strong password. After the password is generated, the user then is able to copy the string and paste it to their own password keeper.",
         image: "",
-        liveLink: "",
-        gitHubLink: "",
-        keywords: ["",""]
+        liveLink: "https://dinozio-design.github.io/03-Password-Generator-JavaScript/",
+        gitHubLink: "https://github.com/dinozio-design/03-Password-Generator-JavaScript",
+        keywords: ["JavaScrip", "Object Oriented Programming (OOP)","CSS", "Media Query", "Mobile First", "Responsive Design", "HTML" ]
     },
     {
         titel: "",
@@ -13,7 +13,7 @@ const ProjectData = [
         image: "",
         liveLink: "",
         gitHubLink: "",
-        keywords: ["",""]
+        keywords: ["", ""]
     },
     {
         titel: "",
@@ -21,7 +21,7 @@ const ProjectData = [
         image: "",
         liveLink: "",
         gitHubLink: "",
-        keywords: ["",""]
+        keywords: ["", ""]
     },
     {
         titel: "",
@@ -29,7 +29,7 @@ const ProjectData = [
         image: "",
         liveLink: "",
         gitHubLink: "",
-        keywords: ["",""]
+        keywords: ["", ""]
     },
     {
         titel: "",
@@ -37,7 +37,7 @@ const ProjectData = [
         image: "",
         liveLink: "",
         gitHubLink: "",
-        keywords: ["",""]
+        keywords: ["", ""]
     },
     {
         titel: "",
@@ -45,9 +45,9 @@ const ProjectData = [
         image: "",
         liveLink: "",
         gitHubLink: "",
-        keywords: ["",""]
+        keywords: ["", ""]
     },
-    
+
 ];
 
 export default ProjectData;
