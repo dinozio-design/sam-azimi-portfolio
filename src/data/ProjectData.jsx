420ca3cda718e97d343ae9fdda1008cdbb08ce13
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-12-11 12:55:24 -0500

    outlining project data structure

---
+++ src/data/ProjectData.jsx
@@ -0,0 +1,53 @@
+const ProjectData = [
+    {
+        titel: "",
+        description: "",
+        image: "",
+        liveLink: "",
+        gitHubLink: "",
+        keywords: ["",""]
+    },
+    {
+        titel: "",
+        description: "",
+        image: "",
+        liveLink: "",
+        gitHubLink: "",
+        keywords: ["",""]
+    },
+    {
+        titel: "",
+        description: "",
+        image: "",
+        liveLink: "",
+        gitHubLink: "",
+        keywords: ["",""]
+    },
+    {
+        titel: "",
+        description: "",
+        image: "",
+        liveLink: "",
+        gitHubLink: "",
+        keywords: ["",""]
+    },
+    {
+        titel: "",
+        description: "",
+        image: "",
+        liveLink: "",
+        gitHubLink: "",
+        keywords: ["",""]
+    },
+    {
+        titel: "",
+        description: "",
+        image: "",
+        liveLink: "",
+        gitHubLink: "",
+        keywords: ["",""]
+    },
+    
+];
+
+export default ProjectData;
