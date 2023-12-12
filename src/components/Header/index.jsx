48e7f064f0ea6ed686006e0a4b58b50f6d65bc3d
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-12-12 11:11:01 -0500

    added header overall style

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,31 @@
 import React from "react";
 import Navbar from "../Navbar";
 
+// styling the header
+const styles = {
+    appHeaderContainer: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: "calc(10px + 2vmin)",
+        border:" thick solid",
+        borderColor: "#22223b",
+        backgroundColor: "#22223b",
+        color:" #f2e9e4",
+        paddingTop:"1.5em"
+    },
+    headerHeading:{
+
+    },
+    headerText:{
+
+    }
+};
 export default function Header() {
     return (
         <>
-            <header className="App-header">
+            <header style = {styles.appHeaderContainer} className="App-header">
                 <h1>SAM AZIMI</h1>
                 <p>JavaScrip Developer</p>
             </header>
