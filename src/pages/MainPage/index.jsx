a44aca793e332194f5c2a1fdd7535b45da37ce98
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-12-12 11:37:08 -0500

    added media query to mainPage

--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -4,14 +4,25 @@ import Footer from "../../components/Footer";
 import Projects from "../Projects";
 import Resume from "../Resume";
 
+const styles ={
+    appMainPageContainer:{
 
+    },
+
+    "@media only screen and (min-width:993px)": {
+        appMainPageContainer: {
+            flex: "3",
+            height: "100vh"
+        }
+    }
+}
 export default function MainPage() {
     return (
-        <>
+        <div style={styles.appMainPageContainer}>
             <About />
             <Projects />
             <Resume />
             <Footer />
-        </>
+        </div>
     );
 };
