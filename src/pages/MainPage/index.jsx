dc0be7bbc3d97780d2ff127df955a6e3e644f9a6
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-12-12 19:38:16 -0500

    rewrote the MainPage for lazyloading

--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -1,77 +1,86 @@
-import React, { useRef, useEffect } from "react";
-import About from "../About";
-import Footer from "../../components/Footer";
-import Projects from "../Projects";
-import Resume from "../Resume";
+import React, { useRef, useEffect, lazy, Suspense } from "react";
 
+const LazyAbout = lazy(() => import("../About"));
+const LazyProjects = lazy(() => import("../Projects"));
+const LazyResume = lazy(() => import("../Resume"));
+const LazyFooter = lazy(() => import("../../components/Footer"));
 
 const styles = {
-    appMainPageContainer: {
-        border: " thick solid",
-        borderColor: "#9a8c98",
-        backgroundColor: "#e6f3f3",
-        padding: "2em"
-    }
-}
+  appMainPageContainer: {
+    border: "thick solid",
+    borderColor: "#9a8c98",
+    backgroundColor: "#e6f3f3",
+    padding: "2em",
+  },
+};
+
 export default function MainPage() {
-    const aboutRef = useRef(null);
-    const projectsRef = useRef(null);
-    const resumeRef = useRef(null);
-    const footerRef = useRef(null);
+  const aboutRef = useRef(null);
+  const projectsRef = useRef(null);
+  const resumeRef = useRef(null);
+  const footerRef = useRef(null);
 
-    useEffect(() => {
-        const aboutObserver = new IntersectionObserver(handleIntersection, {
-            threshold: 0.5,
-        });
+  useEffect(() => {
+    const aboutObserver = new IntersectionObserver(handleIntersection, {
+      threshold: 0.5,
+    });
 
-        const projectsObserver = new IntersectionObserver(handleIntersection, {
-            threshold: 0.5,
-        });
+    const projectsObserver = new IntersectionObserver(handleIntersection, {
+      threshold: 0.5,
+    });
 
-        const resumeObserver = new IntersectionObserver(handleIntersection, {
-            threshold: 0.5,
-        });
+    const resumeObserver = new IntersectionObserver(handleIntersection, {
+      threshold: 0.5,
+    });
 
-        const footerObserver = new IntersectionObserver(handleIntersection, {
-            threshold: 0.5,
-        });
+    const footerObserver = new IntersectionObserver(handleIntersection, {
+      threshold: 0.5,
+    });
 
-        if (aboutRef.current) aboutObserver.observe(aboutRef.current);
-        if (projectsRef.current) projectsObserver.observe(projectsRef.current);
-        if (resumeRef.current) resumeObserver.observe(resumeRef.current);
-        if (footerRef.current) footerObserver.observe(footerRef.current);
+    if (aboutRef.current) aboutObserver.observe(aboutRef.current);
+    if (projectsRef.current) projectsObserver.observe(projectsRef.current);
+    if (resumeRef.current) resumeObserver.observe(resumeRef.current);
+    if (footerRef.current) footerObserver.observe(footerRef.current);
 
-        return () => {
-            aboutObserver.disconnect();
-            projectsObserver.disconnect();
-            resumeObserver.disconnect();
-            footerObserver.disconnect();
-        };
-    }, []);
-
-    const handleIntersection = (entries) => {
-        entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-                // Load content or perform actions when components are in view
-                console.log(`Component ${entry.target.tagName} is in view!`);
-            }
-        });
+    return () => {
+      aboutObserver.disconnect();
+      projectsObserver.disconnect();
+      resumeObserver.disconnect();
+      footerObserver.disconnect();
     };
+  }, []);
 
-    return (
-        <div className="App-mainPage" style={styles.appMainPageContainer}>
-            <div ref={aboutRef}>
-                <About />
-            </div>
-            <div ref={projectsRef}>
-                <Projects />
-            </div>
-            <div ref={resumeRef}>
-                <Resume />
-            </div>
-            <div ref={footerRef}>
-                <Footer />
-            </div>
-        </div>
-    );
+  const handleIntersection = (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        // Load content or perform actions when components are in view
+        console.log(`Component ${entry.target.tagName} is in view!`);
+      }
+    });
+  };
+
+  return (
+    <div className="App-mainPage" style={styles.appMainPageContainer}>
+      <div ref={aboutRef}>
+        <Suspense fallback={<div>Loading About...</div>}>
+          <LazyAbout />
+        </Suspense>
+      </div>
+      <div ref={projectsRef}>
+        <Suspense fallback={<div>Loading Projects...</div>}>
+          <LazyProjects />
+        </Suspense>
+      </div>
+      <div ref={resumeRef}>
+        <Suspense fallback={<div>Loading Resume...</div>}>
+          <LazyResume />
+        </Suspense>
+      </div>
+      <div ref={footerRef}>
+        <Suspense fallback={<div>Loading Footer...</div>}>
+          <LazyFooter />
+        </Suspense>
+      </div>
+    </div>
+  );
 };
