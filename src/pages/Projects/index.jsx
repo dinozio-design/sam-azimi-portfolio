718fe06a8edc866f71ca916ad7f5ab994f60722c
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-12-13 13:42:58 -0500

    cleaned up code

--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -1,6 +1,5 @@
 import React, { useRef } from "react";
 import { Fade, Slide } from "react-awesome-reveal";
-// import Card from "../../layout/Card";
 import ProjectCard from "./ProjectCard";
 import ProjectData from "../../data/ProjectData";
 
@@ -10,18 +9,18 @@ export default function Projects() {
     return (
         <div ref={projectsRef}>
             <Slide triggerOnce direction="right" cascade damping={3}>
-            <h2>
-                My Selected Projects
-            </h2>
+                <h2>
+                    My Selected Projects
+                </h2>
             </Slide>
             <Fade cascade damping={2}>
-            <p>Below you can find a select list of the projects that I have created along with some details about them. All of these projects are open-source and if you follow the GitHub tab, you can find the repository that holds the source code for them. Some of these were class assignments and group projects, and some were just for fun.</p>
+                <p>Below you can find a select list of the projects that I have created along with some details about them. All of these projects are open-source and if you follow the GitHub tab, you can find the repository that holds the source code for them. Some of these were class assignments and group projects, and some were just for fun.</p>
 
-            <div>
-                {ProjectData.map((project, index) => (
-                    <ProjectCard key={index} {...project} />
-                ))}
-            </div>
+                <div>
+                    {ProjectData.map((project, index) => (
+                        <ProjectCard key={index} {...project} />
+                    ))}
+                </div>
             </Fade>
         </div>
     );
--- src/pages/Resume/index.jsx
+++ src/pages/Resume/index.jsx
@@ -1,4 +1,4 @@
-import React, { useRef }  from "react";
+import React, { useRef } from "react";
 import { Zoom } from "react-awesome-reveal";
 import PdfViewer from "./PdfViewer";
 import SamAzimiResume from "./SamAzimiResume.pdf"
@@ -9,10 +9,10 @@ export default function Resume() {
     return (
         <div ref={resumeRef}>
             <Zoom triggerOnce cascade damping={1}>
-            <h2>
-                My Resume
-            </h2>
-            <PdfViewer pdf={SamAzimiResume} />
+                <h2>
+                    My Resume
+                </h2>
+                <PdfViewer pdf={SamAzimiResume} />
             </Zoom>
         </div>
     );
