3637fc762265ea823686b490f4e07399196b9b5c
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-12-15 14:47:08 -0500

    relocated pdf to public

--- src/pages/Resume/index.jsx
+++ src/pages/Resume/index.jsx
@@ -1,7 +1,7 @@
 import React, { useRef } from "react";
 import { Zoom } from "react-awesome-reveal";
 import PdfViewer from "./PdfViewer";
-import SamAzimiResume from "./SamAzimiResume.pdf"
+// import SamAzimiResume from "./SamAzimiResume.pdf"
 
 export default function Resume() {
     const resumeRef = useRef(null);
@@ -12,7 +12,7 @@ export default function Resume() {
                 <h2>
                     My Resume
                 </h2>
-                <PdfViewer pdf={SamAzimiResume} />
+                <PdfViewer  />
             </Zoom>
         </div>
     );
