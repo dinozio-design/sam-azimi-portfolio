a494a73731beae0ceb90a28d7a0e350111fbe612
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-12-18 14:08:27 -0500

    cleaned up mainpage code

--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -22,57 +22,12 @@ const styles = {
   },
 
   fadeIn: {
-    // opacity: 0,
-    // transition: "opacity 0.5s ease-in-out",
     padding: "2em",
 
   },
 };
 
 export default function MainPage() {
-  // const aboutRef = useRef(null);
-  // const projectsRef = useRef(null);
-  // const resumeRef = useRef(null);
-  // const footerRef = useRef(null);
-
-  // useEffect(() => {
-  //   const aboutObserver = new IntersectionObserver(handleIntersection, {
-  //     threshold: 0.5,
-  //   });
-
-  //   const projectsObserver = new IntersectionObserver(handleIntersection, {
-  //     threshold: 0.5,
-  //   });
-
-  //   const resumeObserver = new IntersectionObserver(handleIntersection, {
-  //     threshold: 0.5,
-  //   });
-
-  //   const footerObserver = new IntersectionObserver(handleIntersection, {
-  //     threshold: 0.5,
-  //   });
-
-  //   if (aboutRef.current) aboutObserver.observe(aboutRef.current);
-  //   if (projectsRef.current) projectsObserver.observe(projectsRef.current);
-  //   if (resumeRef.current) resumeObserver.observe(resumeRef.current);
-  //   if (footerRef.current) footerObserver.observe(footerRef.current);
-
-  //   return () => {
-  //     aboutObserver.disconnect();
-  //     projectsObserver.disconnect();
-  //     resumeObserver.disconnect();
-  //     footerObserver.disconnect();
-  //   };
-  // }, []);
-
-  // const handleIntersection = (entries, observer) => {
-  //   entries.forEach((entry) => {
-  //     if (entry.isIntersecting) {
-  //       entry.target.style.opacity = 1;
-  //       observer.unobserve(entry.target);
-  //     }
-  //   });
-  // };
 
   return (
     <div className="App-mainPage" style={styles.appMainPageContainer}>
