c30cb76ff05a4e4112960adfcb0093ce75675b7c
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-12-18 14:06:44 -0500

    removed intersection observer and added reiggerOnce to react reveal

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,10 +1,10 @@
-import React, { useRef } from "react";
+import React from "react";
 
 export default function Footer() {
-    const footerRef = useRef(null);
+ 
 
     return (
-        <div ref={footerRef}>
+        <div>
             <footer className="App-footer">
                 <p></p>
                 <p className='text-body-secondary fst-italic fw-lighter'>This web app is brought to you by:© Sam Azimi - 2023 JavaScrip Developer. Confidential and Proprietary. All Rights Reserved.</p>
--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -22,64 +22,64 @@ const styles = {
   },
 
   fadeIn: {
-    opacity: 0,
-    transition: "opacity 0.5s ease-in-out",
+    // opacity: 0,
+    // transition: "opacity 0.5s ease-in-out",
     padding: "2em",
 
   },
 };
 
 export default function MainPage() {
-  const aboutRef = useRef(null);
-  const projectsRef = useRef(null);
-  const resumeRef = useRef(null);
-  const footerRef = useRef(null);
+  // const aboutRef = useRef(null);
+  // const projectsRef = useRef(null);
+  // const resumeRef = useRef(null);
+  // const footerRef = useRef(null);
 
-  useEffect(() => {
-    const aboutObserver = new IntersectionObserver(handleIntersection, {
-      threshold: 0.5,
-    });
+  // useEffect(() => {
+  //   const aboutObserver = new IntersectionObserver(handleIntersection, {
+  //     threshold: 0.5,
+  //   });
 
-    const projectsObserver = new IntersectionObserver(handleIntersection, {
-      threshold: 0.5,
-    });
+  //   const projectsObserver = new IntersectionObserver(handleIntersection, {
+  //     threshold: 0.5,
+  //   });
 
-    const resumeObserver = new IntersectionObserver(handleIntersection, {
-      threshold: 0.5,
-    });
+  //   const resumeObserver = new IntersectionObserver(handleIntersection, {
+  //     threshold: 0.5,
+  //   });
 
-    const footerObserver = new IntersectionObserver(handleIntersection, {
-      threshold: 0.5,
-    });
+  //   const footerObserver = new IntersectionObserver(handleIntersection, {
+  //     threshold: 0.5,
+  //   });
 
-    if (aboutRef.current) aboutObserver.observe(aboutRef.current);
-    if (projectsRef.current) projectsObserver.observe(projectsRef.current);
-    if (resumeRef.current) resumeObserver.observe(resumeRef.current);
-    if (footerRef.current) footerObserver.observe(footerRef.current);
+  //   if (aboutRef.current) aboutObserver.observe(aboutRef.current);
+  //   if (projectsRef.current) projectsObserver.observe(projectsRef.current);
+  //   if (resumeRef.current) resumeObserver.observe(resumeRef.current);
+  //   if (footerRef.current) footerObserver.observe(footerRef.current);
 
-    return () => {
-      aboutObserver.disconnect();
-      projectsObserver.disconnect();
-      resumeObserver.disconnect();
-      footerObserver.disconnect();
-    };
-  }, []);
+  //   return () => {
+  //     aboutObserver.disconnect();
+  //     projectsObserver.disconnect();
+  //     resumeObserver.disconnect();
+  //     footerObserver.disconnect();
+  //   };
+  // }, []);
 
-  const handleIntersection = (entries, observer) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        entry.target.style.opacity = 1;
-        observer.unobserve(entry.target);
-      }
-    });
-  };
+  // const handleIntersection = (entries, observer) => {
+  //   entries.forEach((entry) => {
+  //     if (entry.isIntersecting) {
+  //       entry.target.style.opacity = 1;
+  //       observer.unobserve(entry.target);
+  //     }
+  //   });
+  // };
 
   return (
     <div className="App-mainPage" style={styles.appMainPageContainer}>
       <section style={styles.spacer}>
         <img style={styles.backGroundImage} src="./images/myWorkImage.png"/> 
       </section>
-      <div ref={aboutRef} style={styles.fadeIn}>
+      <div style={styles.fadeIn}>
         <Suspense fallback={<div>Loading About...</div>}>
           <LazyAbout />
         </Suspense>
@@ -87,19 +87,19 @@ export default function MainPage() {
       <section style={styles.spacer}>
         <img style={styles.backGroundImage} src="./images/backgroundImage.png"/> 
       </section>
-      <div ref={projectsRef} style={styles.fadeIn}>
+      <div style={styles.fadeIn}>
         <Suspense fallback={<div>Loading Projects...</div>}>
           <LazyProjects />
         </Suspense>
       </div>
       <section style={styles.spacer}/>
-      <div ref={resumeRef} style={styles.fadeIn}>
+      <div style={styles.fadeIn}>
         <Suspense fallback={<div>Loading Resume...</div>}>
           <LazyResume />
         </Suspense>
       </div>
       <section style={styles.spacer}/>
-      <div ref={footerRef} style={styles.fadeIn}>
+      <div style={styles.fadeIn}>
         <Suspense fallback={<div>Loading Footer...</div>}>
           <LazyFooter />
         </Suspense>
