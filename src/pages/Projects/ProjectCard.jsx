28c0b11e4fcdf83f2aa4fd899165c6841ee19b94
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-12-20 20:21:43 -0500

    centred content of cards in projects

--- src/pages/Projects/ProjectCard.jsx
+++ src/pages/Projects/ProjectCard.jsx
@@ -5,28 +5,31 @@ import { Tooltip } from "react-tooltip";
 
 const styles = {
     iconStyle: {
-        color: "#7848aa", 
+        color: "#7848aa",
         margin: "4px"
     },
+    bodyText: {
+        textAlign: "justify"
+    }
 };
 
 export default function ProjectCard({ title, description, imageSource, liveLink, gitHubLink, keywords }) {
     return (
-        <div className="card">
+        <div className="card text-center">
             <Fade triggerOnce cascade damping={2}>
                 <div className="card-header text-center">
                     <h4>{title}</h4>
                 </div>
                 <div className="card-body">
                     <img src={imageSource} alt={title} />
-                    <p>{description}</p>
+                    <p style={styles.bodyText}>{description}</p>
                     <div className="project-links">
                         <a href={liveLink} target="_blank" rel="noopener noreferrer" data-tooltip-id="DEP-tooltip" data-tooltip-content="Visit Deployment" data-tooltip-place="bottom">
-                            <BsRocketTakeoff style={styles.iconStyle}/>
-                        </a> <Tooltip id="DEP-tooltip" opacity={.5} style={{fontSize:"12px"}}/>
+                            <BsRocketTakeoff style={styles.iconStyle} />
+                        </a> <Tooltip id="DEP-tooltip" opacity={.5} style={{ fontSize: "12px" }} />
                         <a href={gitHubLink} target="_blank" rel="noopener noreferrer" data-tooltip-id="GHR-tooltip" data-tooltip-content="Visit GitHub Repository" data-tooltip-place="bottom">
-                            <BsGithub style={styles.iconStyle}/>
-                        </a> <Tooltip id="GHR-tooltip" opacity={.5} style={{fontSize:"12px"}}/>
+                            <BsGithub style={styles.iconStyle} />
+                        </a> <Tooltip id="GHR-tooltip" opacity={.5} style={{ fontSize: "12px" }} />
                         <p></p>
                     </div>
                     <div className="project-keywords">
